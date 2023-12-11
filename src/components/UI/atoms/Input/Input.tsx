d3f15db90c1ccbe8e918dd52da1b34cd47391eb7
jiyeong-seo <jen.jyseo@gmail.com>
2023-12-12 01:14:42 +0900

    [코드 추가] Input 컴포넌트 props 추가

--- src/components/UI/atoms/Input/Input.tsx
+++ src/components/UI/atoms/Input/Input.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import * as S from './styled';
-
 export interface InputProps {
+  width: number;
+  height: number;
+  fontSize?: number;
+  isError?: boolean;
+  borderRadius: number;
+  placeholder?: string;
+  backGroundColor?: string;
+  borderColorOnFocus?: boolean;
 }
-
-const Input: React.FC<InputProps> = () => {
-
+const Input: React.FC<InputProps> = ({
+  width,
+  height,
+  isError,
+  placeholder,
+  borderRadius,
+  backGroundColor,
+  borderColorOnFocus,
+  fontSize = 18,
+}) => {
   return (
-      <S.Input />
+    <S.Input
+      width={width}
+      height={height}
+      $isError={isError}
+      $fontSize={fontSize}
+      placeholder={placeholder}
+      $borderRadius={borderRadius}
+      $backGroundColor={backGroundColor}
+      $borderColorOnFocus={borderColorOnFocus}
+    />
   );
 };
-
 export default Input;

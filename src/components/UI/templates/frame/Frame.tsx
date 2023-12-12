325fc49714f59031b24af732c3cb302d2feb629f
sojung <gonaeng20@gmail.com>
2023-12-12 23:31:01 +0900

    [ 코드수정 ] frame 폴더 코드컨벤션 맞춰 수정

--- src/components/UI/templates/frame/Frame.tsx
+++ src/components/UI/templates/frame/Frame.tsx
@@ -1,15 +1,16 @@
-import { S } from './style';
+import React from 'react';
+import * as S from './styled';
 //
 
-export default function Frame() {
+const Frame = () => {
   // const location = useLocation();
   // const isNavVisible = location.pathname === '';
 
   return (
     <S.Container>
-      <S.Main>
-        {/* {isNavVisible } */}
-      </S.Main>
+      <S.Main>{/* {isNavVisible } */}</S.Main>
     </S.Container>
   );
-}
+};
+
+export default Frame;
---
+++ src/components/UI/templates/frame/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #e9ecef;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 375px;
+  height: 100vh;
+  overflow: scroll;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 0px 8px;
+  overflow: scroll;
+`;

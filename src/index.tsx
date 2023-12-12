7ab293aa9b18f6c3903719e35db3999782f40d0a
jiyeong-seo <jen.jyseo@gmail.com>
2023-12-12 23:53:48 +0900

    [코드 삭제] index.tsx GlobalStyle 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { GlobalStyle } from './styles';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-

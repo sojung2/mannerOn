5a4f33ed65490a8e20d743cf26ba3cac3cb63ffa
jiyoung <jen.jyseo@gmail.com>
2023-12-14 04:27:49 +0900

    [폴더 추가/파일 추가] Container 컴포넌트 폴더, 컴포넌트, 스타일 정의

---
+++ src/components/UI/template/Container/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { flexDSB } from '@styles/index';
+
+export const Container = styled.div`
+  width: auto;
+  height: calc(100vh - 50px);
+  ${flexDSB}
+`;

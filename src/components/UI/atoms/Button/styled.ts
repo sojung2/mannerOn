9b7d0f74d15a8b08a0551cbc2e2c20439dfa6124
jiyoung <jen.jyseo@gmail.com>
2023-12-14 01:36:37 +0900

    [코드 수정] Button border style 코드 수정

--- src/components/UI/atoms/Button/styled.ts
+++ src/components/UI/atoms/Button/styled.ts
@@ -18,56 +18,51 @@ export const Button = styled.button<ButtonProps>`
   font-style: normal;
   width: ${({ width }) => width && `${width}px`};
   height: ${({ height }) => height}px;
-  ${({theme}) => theme.fonts.CTA_medium};
+  ${({ theme }) => theme.fonts.CTA_medium};
   border-radius: ${({ $borderRadius }) => $borderRadius}px;
   cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
   background-color: ${({ theme, $backGroundColor, disabled }) => {
     if ($backGroundColor === 'black' && disabled) {
-      return theme.colors.darkgrey
+      return theme.colors.darkgray;
     } else if ($backGroundColor === 'white' && disabled) {
-      return theme.colors.gray20
-    } else if($backGroundColor === "black" && !disabled){
+      return theme.colors.gray20;
+    } else if ($backGroundColor === 'black' && !disabled) {
       return theme.colors.primary;
-    }else if($backGroundColor === "white" && !disabled){
+    } else if ($backGroundColor === 'white' && !disabled) {
       return theme.colors.white;
     }
   }};
- 
-  color: 
-  ${({ theme, $backGroundColor, disabled }) => {
+
+  color: ${({ theme, $backGroundColor, disabled }) => {
     if ($backGroundColor === 'black' && disabled) {
-      return theme.colors.gray40
+      return theme.colors.gray40;
     } else if ($backGroundColor === 'white' && disabled) {
-      return theme.colors.gray30
-    }  
-    else if ($backGroundColor === 'white' && !disabled) {
-      return theme.colors.darkgrey;
-    }else if ($backGroundColor === 'black' && !disabled) {
+      return theme.colors.gray30;
+    } else if ($backGroundColor === 'white' && !disabled) {
+      return theme.colors.darkgray;
+    } else if ($backGroundColor === 'black' && !disabled) {
       return theme.colors.white;
     }
-    }
-  }
+  }};
 
   border: ${({ theme, $backGroundColor, disabled }) => {
-    if ($backGroundColor === "white" && !disabled) {
-      return theme.colors.darkgrey;
-    } else if (disabled){
-      return theme.colors.gray30;
-    }else{
-      return "none";
+    if ($backGroundColor === 'white' && !disabled) {
+      return `1px solid ${theme.colors.darkgray}`;
+    } else if (disabled) {
+      return `1px solid ${theme.colors.gray30}`;
+    } else {
+      return 'none';
     }
   }};
 
   &:hover {
     background-color: ${({ theme, $backGroundColor, disabled }) => {
-    if ($backGroundColor === "black" && !disabled) {
-      return theme.colors.darkgrey;
-    } else if ($backGroundColor === "white" && !disabled) {
-      return theme.colors.gray20;
-    }
-
-  }};
-   
+      if ($backGroundColor === 'black' && !disabled) {
+        return theme.colors.darkgray;
+      } else if ($backGroundColor === 'white' && !disabled) {
+        return theme.colors.gray20;
+      }
+    }};
   }
   &:active {
   }

afe37a99eaef2842d91f1fa22cf200ddfcd58ca2
jiyeong-seo <jen.jyseo@gmail.com>
2023-12-13 23:54:40 +0900

    [코드 추가] SvgWrapper 컴포넌트 export 코드 추가

--- src/components/UI/atoms/index.ts
+++ src/components/UI/atoms/index.ts
@@ -1,3 +1,5 @@
+export { default as Box } from './Box';
+export { default as Text } from './Text';
 export { default as Input } from './Input';
 export { default as Button } from './Button';
-export { default as Text } from './Text';
+export { default as SvgWrapper } from './SvgWrapper';

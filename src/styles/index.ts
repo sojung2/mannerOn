f6d64dc5166149ff7ff389b1696160bdc7d52082
jiyeong-seo <jen.jyseo@gmail.com>
2023-12-13 23:47:32 +0900

    [파일 추가] styles/common 파일 추가

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,14 @@
 export { default as GlobalStyle } from './globalStyle';
 export { default as theme } from './theme';
+export {
+  flexCC,
+  flexEC,
+  flexDCC,
+  flexDJC,
+  flexDAC,
+  flexSBC,
+  flexSBS,
+  flexJustifyCenter,
+  flexFlexStartCenter,
+  flexAlignItemsCenter,
+} from './common';

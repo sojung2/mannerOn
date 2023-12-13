16b24cc7602a79c8a86984bc24a780430250d34a
jiyeong-seo <jen.jyseo@gmail.com>
2023-12-13 23:47:32 +0900

    [파일 추가] styles/common 파일 추가

---
+++ src/styles/common.ts
@@ -0,0 +1,60 @@
+import { css } from 'styled-components';
+
+export const flexCC = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const flexJustifyCenter = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export const flexAlignItemsCenter = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const flexDCC = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const flexDJC = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const flexDAC = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const flexEC = css`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const flexFlexStartCenter = css`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const flexSBC = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const flexSBS = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+`;

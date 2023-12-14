959177f4cd827bfc751e60dbd0d28cf42c93c220
jiyoung <jen.jyseo@gmail.com>
2023-12-14 16:36:27 +0900

    [코드 추가] SignInStep 컴포넌트 default 파일 export 코드 추가

--- src/components/SignInStep/index.ts
+++ src/components/SignInStep/index.ts
@@ -2,3 +2,5 @@ export { default as SignInNickName } from './SignInNickName';
 export { default as SignInGender } from './SignInGender';
 export { default as SignInJob } from './SignInJob';
 export { default as SignInAge } from './SignInAge';
+export { default as SignInSuccess } from './SignInSuccess';
+export { default as SignIn } from './SignIn';

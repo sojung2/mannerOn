46b480745693a9e484ba9876d416a082f1812bdf
jiyoung <jen.jyseo@gmail.com>
2023-12-14 21:16:42 +0900

    [코드 추가] Container 컴포넌트 스타일 관련 코드 추가

--- src/components/UI/template/Container/Container.tsx
+++ src/components/UI/template/Container/Container.tsx
@@ -3,10 +3,11 @@ import * as S from './styled';
 
 export interface ContainerProps {
   children?: React.ReactNode;
+  display?: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <S.Container>{children}</S.Container>;
+const Container: React.FC<ContainerProps> = ({ children, display = 'flexDSB' }) => {
+  return <S.Container $display={display}>{children}</S.Container>;
 };
 
 export default Container;

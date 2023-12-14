7a8ac7dd4a5b89b6ce904eac71fb3ce440a9e4fd
jiyoung <jen.jyseo@gmail.com>
2023-12-14 14:33:09 +0900

    [폴더/파일 추가] SignInStepPage 관련 컴포넌트 폴더, 파일 추가

---
+++ src/components/SignInStep/index.ts
@@ -0,0 +1,4 @@
+export { default as SignInNickName } from './SignInNickName';
+export { default as SignInGender } from './SignInGender';
+export { default as SignInJob } from './SignInJob';
+export { default as SignInAge } from './SignInAge';

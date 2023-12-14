96b6ca07ff56dee11bdd9e9f469c514f6c8bfacd
jiyoung <jen.jyseo@gmail.com>
2023-12-14 14:31:40 +0900

    [파일 추가] SignInAge 컴포넌트 생성

---
+++ src/components/SignInStep/SignInAge.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Box, Text, Button } from '@UI/atoms';
+
+interface SignInAgeProps {
+  current: number;
+  setCurrent: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const buttonList = ['10대', '20~14세', '25~29세', '30~34세', '35세 이상'];
+
+const SignInAge: React.FC<SignInAgeProps> = ({ current, setCurrent }) => {
+  return (
+    <Box fontWeight={500}>
+      <Box fontSize={24} fontWeight={700} padding={'0 0 40px 0'}>
+        <Text fontSize={24} fontWeight={700} color={'secondary'}>
+          연령대
+        </Text>
+        를 선택해주세요!
+      </Box>
+      <Box display={'flexCW'} gap={8}>
+        {buttonList.map((gender, i) => (
+          <Button
+            key={i}
+            width={160}
+            height={46}
+            color={current === i ? 'white' : 'gray50'}
+            backgroundColor={current === i ? 'secondary' : 'gray30'}
+            onClick={() => setCurrent(i)}>
+            {gender}
+          </Button>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default SignInAge;

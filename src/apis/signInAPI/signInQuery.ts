549a483190c9b1c2c6403d2526d9b0835085e217
jiyoung <jen.jyseo@gmail.com>
2023-12-15 19:59:57 +0900

    [파일 추가] 이메일 중복 검사 Query 파일 추가

--- src/apis/signInAPI/signInQuery.ts
+++ src/apis/signInAPI/signInQuery.ts
@@ -1,19 +1,33 @@
 import { AxiosError } from 'axios';
-import { useQuery, useMutation, UseMutationOptions } from '@tanstack/react-query';
-import { SignInReq, SignInRes } from '../../types/signUp';
+import { useQuery, useMutation, UseMutationOptions, UseQueryResult } from '@tanstack/react-query';
+import { KEY } from '@shared/index';
 import { BusinessErrorResponse, HTTPResponse } from '../../types/common';
+import { SignUpReq, SignUpRes, SignUpEmailDuplicateReq, SignUpEmailDuplicateRes } from '../../types/signUp';
 import SignInApi from './signInAPI';
 
 const signInApi = new SignInApi();
 
+export const useGetEmailDuplicateQuery = (
+  variable: SignUpEmailDuplicateReq,
+): UseQueryResult<HTTPResponse<SignUpEmailDuplicateRes>> => {
+  return useQuery({
+    queryKey: KEY.SIGNUP.EMAIL_DUPLICATE(variable),
+    queryFn: async () => {
+      return await signInApi.getEmailDuplicate(variable);
+    },
+    enabled: !!variable.email,
+    retry: 1,
+  });
+};
+
 export const usePostSignUpMutation = (
-  useMutationOptions?: UseMutationOptions<HTTPResponse<SignInRes> | BusinessErrorResponse, AxiosError, SignInReq, unknown>,
+  useMutationOptions?: UseMutationOptions<HTTPResponse<SignUpRes> | BusinessErrorResponse, AxiosError, SignUpReq, unknown>,
 ) => {
   const mutationKey = ['SIGNUP_POST'];
 
   const result = useMutation({
     mutationKey,
-    mutationFn: async (variable: SignInReq) => {
+    mutationFn: async (variable: SignUpReq) => {
       return await signInApi.postSignUp(variable);
     },
     ...useMutationOptions,

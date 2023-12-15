54965013c3d2e012891ad88c982e495e22ec816e
jiyoung <jen.jyseo@gmail.com>
2023-12-16 08:03:59 +0900

    [코드 수정] ChatBox 컴포넌트 코드 수정

--- src/components/UI/atoms/ChatBox/ChatBox.tsx
+++ src/components/UI/atoms/ChatBox/ChatBox.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
 import * as S from './styled';
-import { useFormContext } from 'react-hook-form';
-import {ChatIcons} from '@UI/molecules/ChatIcons';
 import { SvgWrapper } from '@UI/atoms';
-import profile from '@assets/logo/aiProfileLogo.svg'
+import { ChatIcons } from '@UI/molecules/ChatIcons';
+import profile from '@assets/logo/aiProfileLogo.svg';
 
 export interface ChatBoxProps {
   role?: 'user' | 'ai';
   isError?: boolean;
   backGroundColor?: string;
-  onFocus?: React.FocusEventHandler<HTMLInputElement>;
   children?: React.ReactNode;
+  onFocus?: React.FocusEventHandler<HTMLInputElement>;
 }
 
-const ChatBox: React.FC<ChatBoxProps> = ({
-  role = 'user',
-  isError,
-  onFocus,
-  children,
-}) => { 
-  const { register } = useFormContext();
-  
+const ChatBox: React.FC<ChatBoxProps> = ({ role = 'user', isError, onFocus, children }) => {
   return (
     <>
-      {role === 'ai' &&
-      <SvgWrapper width={33} height={32} style={{marginBottom: '15px'}} svg={profile} />}
-      <S.ChatBox
-        $isError={isError}
-        $borderRadius={0}
-        role={role}
-        onFocus={onFocus}
-      >
+      {role === 'ai' && <SvgWrapper width={33} height={32} svg={profile} />}
+      <S.ChatBox $isError={isError} $borderRadius={0} role={role} onFocus={onFocus}>
         {children}
-        {role === 'ai' && <ChatIcons/>}
+        {role === 'ai' && <ChatIcons />}
       </S.ChatBox>
     </>
   );

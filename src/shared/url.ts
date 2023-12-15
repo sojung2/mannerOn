bad6c8022ba5e992c174e60607608757a787d5ca
jiyoung <jen.jyseo@gmail.com>
2023-12-15 21:45:26 +0900

    [코드 추가] 로그인 API Url 추가

--- src/shared/url.ts
+++ src/shared/url.ts
@@ -1,5 +1,9 @@
 const API_URL = {
-  SIGNIN: { SIGNUP: '/api/users/signup' as const, EMAIL_DUPLICATE: 'api/users/check-email' as const },
+  SIGNIN: {
+    SIGNUP: '/api/users/signup' as const,
+    SIGNIN: '/api/users/login' as const,
+    EMAIL_DUPLICATE: 'api/users/check-email' as const,
+  },
 };
 
 export default API_URL;

8b6159bdfe130c4e512882711ac24573d14de9d4
jiyoung <jen.jyseo@gmail.com>
2023-12-16 08:11:20 +0900

    [코드 추가] Url 추가

--- src/shared/url.ts
+++ src/shared/url.ts
@@ -4,6 +4,10 @@ const API_URL = {
     SIGNIN: '/api/users/login' as const,
     EMAIL_DUPLICATE: 'api/users/check-email' as const,
   },
+  CHAT: {
+    CHAT_START: '/api/chats/start' as const,
+    CHAT: (chatroomId: number) => `/api/chats/${chatroomId}` as const,
+  },
 };
 
 export default API_URL;

b05b66c1d02c69567dca2e4e9a2f5d33d9ed5968
jiyoung <jen.jyseo@gmail.com>
2023-12-15 20:06:17 +0900

    [코드 추가] url 파일 이메일 중복 체크 Url 코드 추가

--- src/shared/url.ts
+++ src/shared/url.ts
@@ -1,5 +1,5 @@
 const API_URL = {
-  SIGNIN: { SIGNUP: '/api/users/signup' as const },
+  SIGNIN: { SIGNUP: '/api/users/signup' as const, EMAIL_DUPLICATE: 'api/users/check-email' as const },
 };
 
 export default API_URL;

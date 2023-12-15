693d11d479eb429429965172b7ddee31eed9ea81
jiyoung <jen.jyseo@gmail.com>
2023-12-15 14:04:49 +0900

    [파일 추가] API URL 파일 추가

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,2 +1,3 @@
 export { default as Router } from './Router';
 export { default as REGEX } from './regex';
+export { default as API_URL } from './url';
---
+++ src/shared/url.ts
@@ -0,0 +1,5 @@
+const API_URL = {
+  SIGNIN: { SIGNUP: '/api/users/signup' as const },
+};
+
+export default API_URL;

ff494d4786965911fd2686a47af12a2b8382471a
jiyoung <jen.jyseo@gmail.com>
2023-12-15 20:03:35 +0900

    [코드 삭제] Splash 페이지 사용하지 않는 코드 제거

--- src/pages/Splash/SplashPage.tsx
+++ src/pages/Splash/SplashPage.tsx
@@ -1,20 +1,19 @@
-import { Frame } from "@UI/template";
-import * as S from "./styled"
-import WhiteBigLogo from '@assets/logo/WhiteBigLogo.svg'
+import * as S from './styled';
+import WhiteBigLogo from '@assets/logo/WhiteBigLogo.svg';
 
 const SplashPage = () => {
-    console.log("splash")
-    return (
-        <S.Splash>
-            <S.Wrapper>
-                <S.Title>답장이 고민될 때 매너<S.Highlight>ON</S.Highlight></S.Title>
-                <S.ImgWrapper>
-                <img src={WhiteBigLogo} alt="logo"/>
-                </S.ImgWrapper>
-            </S.Wrapper>
-        </S.Splash>
-
-    );
+  return (
+    <S.Splash>
+      <S.Wrapper>
+        <S.Title>
+          답장이 고민될 때 매너<S.Highlight>ON</S.Highlight>
+        </S.Title>
+        <S.ImgWrapper>
+          <img src={WhiteBigLogo} alt="logo" />
+        </S.ImgWrapper>
+      </S.Wrapper>
+    </S.Splash>
+  );
 };
 
 export default SplashPage;

633084a3bd8b0e310da28de05284a19c625aeb33
jiyoung <jen.jyseo@gmail.com>
2023-12-15 21:46:41 +0900

    [UI 수정] Input 컴포넌트 UI 수정

--- src/pages/Login/styled.ts
+++ src/pages/Login/styled.ts
@@ -5,9 +5,7 @@ export const Login = styled.section`
 `;
 
 export const WrapperTop = styled.div`
-    color: ${({theme}) => theme.colors.darkgrey};
-
-    
+  color: ${({ theme }) => theme.colors.darkgrey};
 `;
 export const ImgWrapper = styled.div`
   padding-bottom: 24px;
@@ -24,10 +22,10 @@ export const LoginSubTitle = styled.p`
 
 export const WrapperCenter = styled.div`
   padding-top: 48px;
-
-  Input {
-    margin-bottom: 12px;
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
 `;
 
 export const JoinText = styled.p`

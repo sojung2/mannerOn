af8c179e2828c63c5520ebdcbe65cf7aca602d29
jiyoung <jen.jyseo@gmail.com>
2023-12-16 08:10:37 +0900

    [UI 추가] Home 페이지 style 컴포넌트 신규 정의

--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -8,3 +8,12 @@ export const WrapperTop = styled.div`
   color: ${({ theme }) => theme.colors.darkgray};
   padding: 17px 0px 24px 0px;
 `;
+
+export const ChatWrapper = styled.div`
+  height: calc(100vh - 50px);
+  overflow: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;

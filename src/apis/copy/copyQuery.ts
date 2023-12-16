3e050dbb5e7049da29b8ac1baab71902e6343044
jiyoung <jen.jyseo@gmail.com>
2023-12-16 18:22:23 +0900

    [코드 수정] 복사 Query 수정

--- src/apis/copy/copyQuery.ts
+++ src/apis/copy/copyQuery.ts
@@ -1,23 +1,22 @@
 import { AxiosError } from 'axios';
 import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { KEY } from '@shared/index';
-import { IconRes } from '../../types/feedback';
+import { IconRes, IconReq } from '../../types/feedback';
 import { BusinessErrorResponse, HTTPResponse } from '../../types/common';
 import CopyApi from './copyAPI';
 
 const copyApi = new CopyApi();
 
-export const usePutCopyMutation = (
-    chatroomId: number,
-    useMutationOptions?: UseMutationOptions<HTTPResponse<IconRes> | BusinessErrorResponse, AxiosError, unknown>,
-  ) => {
-    const mutationKey = KEY.COPY.COPY_PUT;
-    const result = useMutation({
-      mutationKey,
-      mutationFn: async () => {
-        return await copyApi.putCopy(chatroomId);
-      },
-      ...useMutationOptions,
-    });
-    return { ...result, mutationKey };
-  };
+export const usePutCopyMessageMutation = (
+  useMutationOptions?: UseMutationOptions<HTTPResponse<IconRes> | BusinessErrorResponse, AxiosError, IconReq, unknown>,
+) => {
+  const mutationKey = KEY.COPY.COPY_PUT;
+  const result = useMutation({
+    mutationKey,
+    mutationFn: async (params: IconReq) => {
+      return await copyApi.putCopyMessage(params?.chatId);
+    },
+    ...useMutationOptions,
+  });
+  return { ...result, mutationKey };
+};

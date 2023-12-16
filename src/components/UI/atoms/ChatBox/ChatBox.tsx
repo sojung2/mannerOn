f66d406a7ef22eb64acaa3121a3ad94eb8ce5958
sojung <gonaeng20@gmail.com>
2023-12-16 15:27:42 +0900

    [기능추가] copy Icon 클릭시 클립보드에 복사기능구현

--- src/components/UI/atoms/ChatBox/ChatBox.tsx
+++ src/components/UI/atoms/ChatBox/ChatBox.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import * as S from './styled';
 import { SvgWrapper } from '@UI/atoms';
 import { ChatIcons } from '@UI/molecules/ChatIcons';
 import profile from '@assets/logo/aiProfileLogo.svg';
+
 
 export interface ChatBoxProps {
   role?: 'user' | 'ai';
@@ -10,15 +12,25 @@ export interface ChatBoxProps {
   backGroundColor?: string;
   children?: React.ReactNode;
   onFocus?: React.FocusEventHandler<HTMLInputElement>;
+  text?: string | undefined;
 }
 
-const ChatBox: React.FC<ChatBoxProps> = ({ role = 'user', isError, onFocus, children }) => {
+
+const ChatBox: React.FC<ChatBoxProps> = ({ role, isError, onFocus, children }) => {
+  let childrenString: string | undefined;
+  if (typeof children === 'string') {
+    childrenString = children;
+  } else if (React.isValidElement(children)) {
+    childrenString = ReactDOMServer.renderToStaticMarkup(children);
+  } else {
+    childrenString = undefined;
+  }
   return (
     <>
       {role === 'ai' && <SvgWrapper width={33} height={32} svg={profile} />}
       <S.ChatBox $isError={isError} $borderRadius={0} role={role} onFocus={onFocus}>
-        {children}
-        {role === 'ai' && <ChatIcons />}
+          {children}
+        {role === 'ai' && <ChatIcons text={childrenString} />}
       </S.ChatBox>
     </>
   );
--- src/components/UI/molecules/ChatIcons/ChatIcons.tsx
+++ src/components/UI/molecules/ChatIcons/ChatIcons.tsx
@@ -1,25 +1,39 @@
 import React, { useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import * as S from './styled';
 import CopyIcon from '@assets/icon/copyIcon'
 import LikeIcon from '@assets/icon/likeIcon'
 import DislikeIcon from '@assets/icon/dislikeIcon'
 import { Modal } from '@UI/atoms/Modal';
 
+
 export interface ChatIconProps {
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  text?: string | undefined;
 }
 
 const ChatIcons: React.FC<ChatIconProps> = ({
   onChange,  
   onClick,
-
+  text,
 }) => {
 
   const [copyClicked, setcopyClicked] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
   const [likeIconClicked, setLikeIconClicked] = useState(false);
   const [dislikeIconClicked, setDislikeIconClicked] = useState(false);
+
+
+  let textString: string | undefined;
+if (typeof text === 'string') {
+  textString = text;
+} else if (React.isValidElement(text)) {
+  textString = ReactDOMServer.renderToStaticMarkup(text);
+} else {
+  textString = undefined;
+}
 
   const handleClick = () => {
     setcopyClicked(!copyClicked);
@@ -42,12 +56,15 @@ const ChatIcons: React.FC<ChatIconProps> = ({
       setModalOpen(true);
     };
 
-
   return (
     <S.ChatIcons onChange={onChange}>
       <S.StyledCopyIcon>
-        <CopyIcon isClicked={copyClicked} onClick={handleClick} />
-        {modalOpen && <Modal setModalOpen={setModalOpen} />}
+        <CopyToClipboard text={textString || ''} onCopy={handleClick}>
+          <div>
+          <CopyIcon isClicked={copyClicked} />
+          </div>
+        </CopyToClipboard>
+          {modalOpen && <Modal setModalOpen={setModalOpen} />}
       </S.StyledCopyIcon>
       <S.StyledLikeIcon>
         <LikeIcon isClicked={likeIconClicked} onClick={handleLikeIconClick}/>
